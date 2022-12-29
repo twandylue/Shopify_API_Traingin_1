5ca5dd0ceebcdf7b17b7f5bafa1eb03387335f90
AndyLu <laserlue@gmail.com>
2022-12-29 16:26:18 +0800

    feat: first step in main.rs

--- shopify_api_client_cli/src/main.rs
+++ shopify_api_client_cli/src/main.rs
@@ -1,3 +1,17 @@
+use std::io::{stdin, stdout, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let _ = stdout().flush();
+    let mut pattern = String::new();
+    let mut path = String::new();
+    stdin()
+        .read_line(&mut pattern)
+        .expect("Did not enter a correct string");
+
+    stdin()
+        .read_line(&mut path)
+        .expect("Did not enter a correct string");
+
+    println!("pattern: {}", pattern);
+    println!("path: {}", path);
 }
