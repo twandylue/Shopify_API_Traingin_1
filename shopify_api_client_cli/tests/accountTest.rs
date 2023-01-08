2e26de812192a133105888f7504f1f645ff75534
AndyLu <laserlue@gmail.com>
2023-01-08 12:44:09 +0800

    test: add more unit test cases in accountTest.rs

--- shopify_api_client_cli/tests/accountTest.rs
+++ shopify_api_client_cli/tests/accountTest.rs
@@ -1,7 +1,7 @@
 use shopify_api_client_cli::models::account::{Account, State};
 
 #[test]
-fn test_account_login_change_state_successful() {
+fn test_account_login_change_state_ok() {
     // arrange
     let expected = State::Logined;
     let email = "andylu@email.com".to_string();
@@ -13,4 +13,34 @@ fn test_account_login_change_state_successful() {
 
     // assert
     assert_eq!(expected, account.state());
+}
+
+#[test]
+fn test_account_get_name_ok() {
+    // arrange
+    let expected = "andylu@email.com".to_string();
+    let email = expected.clone();
+    let password = "123456789".to_string();
+    let account = Account::new(email, password);
+
+    // act
+    let actual = account.email();
+
+    // assert
+    assert_eq!(expected, actual);
+}
+
+#[test]
+fn test_account_get_state_ok() {
+    // arrange
+    let expected = State::Init;
+    let email = "andylu@email.com".to_string();
+    let password = "123456789".to_string();
+    let account = Account::new(email, password);
+
+    // act
+    let actual = account.state();
+
+    // assert
+    assert_eq!(expected, actual);
 }
