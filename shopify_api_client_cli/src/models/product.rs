689fc7a5b2c590d59ae5897146d2af17e36c1cb0
AndyLu <laserlue@gmail.com>
2023-01-10 02:21:26 +0800

    feat: more associated functions in product

--- shopify_api_client_cli/src/models/product.rs
+++ shopify_api_client_cli/src/models/product.rs
@@ -1,10 +1,34 @@
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Product {
-    Id: i32,
+    id: u32,
+    name: String,
+    price: u32,
+    description: String,
 }
 
 impl Product {
-    pub fn id(&self) -> i32 {
-        self.Id
+    pub fn new(id: u32, name: String, price: u32, description: String) -> Self {
+        Product {
+            id,
+            name,
+            price,
+            description,
+        }
+    }
+
+    pub fn id(&self) -> u32 {
+        self.id
+    }
+
+    pub fn name(&self) -> String {
+        self.name.clone()
+    }
+
+    pub fn description(&self) -> String {
+        self.description.clone()
+    }
+
+    pub fn price(&self) -> u32 {
+        self.price
     }
 }
