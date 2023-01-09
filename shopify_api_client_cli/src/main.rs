0f323a27a2e8bd02288c1ab2fb16cf954925d3af
AndyLu <laserlue@gmail.com>
2023-01-09 08:15:34 +0800

    feat: update process in main.rs

--- shopify_api_client_cli/src/main.rs
+++ shopify_api_client_cli/src/main.rs
@@ -1,17 +1,35 @@
+mod graphqls;
+mod models;
+
+use clap::Parser;
+use shopify_api_client_cli::models::account::Account;
 use std::io::{stdin, stdout, Write};
 
+#[derive(Parser, Debug)]
+struct Cli {
+    account: String,
+    password: String,
+}
+
 fn main() {
+    // let args = Cli::parse();
+    println!("Hi! Please Login First");
     let _ = stdout().flush();
-    let mut pattern = String::new();
-    let mut path = String::new();
+    println!("- Account: ");
+    let mut email = String::new();
+    let mut password = String::new();
     stdin()
-        .read_line(&mut pattern)
+        .read_line(&mut email)
+        .expect("Did not enter a correct string");
+    println!("- Password: ");
+    stdin()
+        .read_line(&mut password)
         .expect("Did not enter a correct string");
 
-    stdin()
-        .read_line(&mut path)
-        .expect("Did not enter a correct string");
+    let account = Account::new(email, password);
+    println!();
+    println!("Login Success! Hi! {}", account.email());
 
-    println!("pattern: {}", pattern);
-    println!("path: {}", path);
+    println!("#####################################################");
+    println!("Start Conversation...");
 }
