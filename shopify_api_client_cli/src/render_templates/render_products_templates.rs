3520734928b4ad13b540f415d00aafd475e2723c
AndyLu <laserlue@gmail.com>
2023-01-10 09:30:14 +0800

    refactor: modify println format in render_products_templates

--- shopify_api_client_cli/src/render_templates/render_products_templates.rs
+++ shopify_api_client_cli/src/render_templates/render_products_templates.rs
@@ -1,6 +1,8 @@
 use shopify_api_client_cli::models::product_list::Product_List;
 
 pub fn render_products_info(product_list: &Product_List) {
+    println!();
+    println!("{}", format!("{:-<45}", ""));
     println!("Unifi 團購清單");
     product_list.items().iter().for_each(|product| {
         println!(
@@ -11,6 +13,6 @@ pub fn render_products_info(product_list: &Product_List) {
             product.description()
         );
     });
-    println!("----------------------------------");
+    println!("{}", format!("{:-<45}", ""));
     println!("Please choose what's you want(id).");
 }
