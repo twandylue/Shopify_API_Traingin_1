7844833b318a782d512936ad9d209d8c32a834f2
AndyLu <laserlue@gmail.com>
2023-01-10 11:58:10 +0800

    refactor: add new cli steps

--- shopify_api_client_cli/src/cli/cli_steps.rs
+++ shopify_api_client_cli/src/cli/cli_steps.rs
@@ -1,9 +1,105 @@
 // TODO:
 
-pub fn first_step_login() {}
+use std::io::stdin;
 
-pub fn second_step_what_do_you_want() {}
+use shopify_api_client_cli::models::{
+    account::{Account, State},
+    cart::Cart,
+    product_list::Product_List,
+};
 
-pub fn third_step_selecting_products() {}
+use crate::render_templates::{render_cart_templates, render_products_templates};
 
-pub fn forth_step_checking_selected_products() {}
+pub fn first_step_login() -> Account {
+    println!("Hi! Please Login First");
+    println!("- Account: ");
+    let mut email = String::new();
+    let mut password = String::new();
+    stdin()
+        .read_line(&mut email)
+        .expect("Did not enter a correct string");
+    println!("- Password: ");
+    stdin()
+        .read_line(&mut password)
+        .expect("Did not enter a correct string");
+
+    let mut account = Account::new(email, password);
+    account.login();
+
+    return account;
+}
+
+pub fn second_step_what_do_you_want_to_do() {
+    println!("What do you want to do?");
+    println!("- whatsnew");
+    println!("- nothing");
+
+    let mut decision = String::new();
+    stdin()
+        .read_line(&mut decision)
+        .expect("Did not enter a correct string");
+
+    if !decision.trim_end().eq("whatsnew") {
+        println!("Ok! I can do nothing. ByeBye!");
+        return;
+    }
+}
+
+pub fn third_step_selecting_products(account: &mut Account) -> Cart {
+    let list = Product_List::new();
+    let mut cart: Cart = Cart::new();
+    account.select_products();
+    while account.state() == State::SelectingProducts {
+        render_products_templates::render_products_info(&list);
+        println!("input 'x' to check your cart");
+        let mut input = String::new();
+        stdin()
+            .read_line(&mut input)
+            .expect("Did not enter a correct string");
+        if input.trim_end().eq("x") {
+            account.check_selected_products();
+            break;
+        }
+
+        let ele = list
+            .items()
+            .into_iter()
+            .find(|x| x.id() == input.trim_end().parse::<u32>().unwrap());
+
+        cart.add(ele.unwrap());
+        render_cart_templates::render_cart_info(&cart);
+    }
+
+    return cart;
+}
+
+pub fn forth_step_checking_selected_products(cart: Cart, account: &mut Account) -> Cart {
+    let mut final_cart = cart.clone();
+    while account.state() == State::CheckingSelectedProducts {
+        println!("Checking your personal cart...");
+        render_cart_templates::render_cart_info(&final_cart);
+        println!("Please input item number to 'remove' the product from your personal cart.");
+        println!("Or input 'x' to confirm your personal cart.");
+        let mut input = String::new();
+        stdin()
+            .read_line(&mut input)
+            .expect("Did not enter a correct string");
+        if input.trim_end().eq("x") {
+            break;
+        }
+
+        let item = input.trim_end().parse::<u32>().unwrap();
+
+        let product_list = Product_List::new();
+        if let Some(product) = product_list.items().into_iter().find(|p| p.id() == item) {
+            final_cart.remove(product);
+        } else {
+            unreachable!(
+                "product id(in your cart): {} is not on the product list.",
+                item
+            );
+        }
+    }
+
+    return final_cart;
+}
--- shopify_api_client_cli/src/main.rs
+++ shopify_api_client_cli/src/main.rs
@@ -3,14 +3,14 @@ mod graphqls;
 mod models;
 mod render_templates;
 
-use crate::render_templates::{render_cart_templates, render_products_templates};
-use clap::Parser;
-use shopify_api_client_cli::models::{
-    account::{Account, State},
-    cart::Cart,
-    product_list::Product_List,
+use crate::{
+    cli::cli_steps::first_step_login,
+    cli::cli_steps::second_step_what_do_you_want_to_do,
+    cli::cli_steps::{forth_step_checking_selected_products, third_step_selecting_products},
 };
-use std::io::{stdin, stdout, Write};
+use clap::Parser;
+
+use std::io::{stdout, Write};
 
 #[derive(Parser, Debug)]
 struct Cli {
@@ -20,91 +20,21 @@ struct Cli {
 
 fn main() {
     // let args = Cli::parse();
-    println!("Hi! Please Login First");
     let _ = stdout().flush();
-    println!("- Account: ");
-    let mut email = String::new();
-    let mut password = String::new();
-    stdin()
-        .read_line(&mut email)
-        .expect("Did not enter a correct string");
-    println!("- Password: ");
-    stdin()
-        .read_line(&mut password)
-        .expect("Did not enter a correct string");
 
-    let mut account = Account::new(email, password);
-    account.login();
+    let mut account = first_step_login();
 
     println!();
     println!("Login Success! Hi! {}", account.email());
 
     println!("#####################################################");
     println!("Start Conversation...");
-    println!("What do you want to do?");
-    println!("- whatsnew");
-    println!("- nothing");
 
-    let mut decision = String::new();
-    stdin()
-        .read_line(&mut decision)
-        .expect("Did not enter a correct string");
+    second_step_what_do_you_want_to_do();
 
-    if !decision.trim_end().eq("whatsnew") {
-        println!("Ok! I can do nothing. ByeBye!");
-        return;
-    }
+    let cart = third_step_selecting_products(&mut account);
 
-    let list = Product_List::new();
-    let mut cart: Cart = Cart::new();
-    account.select_products();
-    while account.state() == State::SelectingProducts {
-        render_products_templates::render_products_info(&list);
-        println!("input 'x' to check your cart");
-        let mut input = String::new();
-        stdin()
-            .read_line(&mut input)
-            .expect("Did not enter a correct string");
-        if input.trim_end().eq("x") {
-            account.check_selected_products();
-            break;
-        }
-
-        let ele = list
-            .items()
-            .into_iter()
-            .find(|x| x.id() == input.trim_end().parse::<u32>().unwrap());
-
-        cart.add(ele.unwrap());
-        render_cart_templates::render_cart_info(&cart);
-    }
-
-    let mut final_cart = cart.clone();
-    while account.state() == State::CheckingSelectedProducts {
-        println!("Checking your personal cart...");
-        render_cart_templates::render_cart_info(&final_cart);
-        println!("Please input item number to 'remove' the product from your personal cart.");
-        println!("Or input 'x' to confirm your personal cart.");
-        let mut input = String::new();
-        stdin()
-            .read_line(&mut input)
-            .expect("Did not enter a correct string");
-        if input.trim_end().eq("x") {
-            break;
-        }
-
-        let item = input.trim_end().parse::<u32>().unwrap();
-
-        let product_list = Product_List::new();
-        if let Some(product) = product_list.items().into_iter().find(|p| p.id() == item) {
-            final_cart.remove(product);
-        } else {
-            unreachable!(
-                "product id(in your cart): {} is not on the product list.",
-                item
-            );
-        }
-    }
+    let final_cart = forth_step_checking_selected_products(cart, &mut account);
 
     // TODO:
     // while account.state() == State::CreatingConsumer {
