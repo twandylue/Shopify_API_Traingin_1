70cf307aa374cd1af13e1c50b86e3f4d4d4fb74c
AndyLu <laserlue@gmail.com>
2023-01-10 09:43:50 +0800

    feat: init for cli_steps

---
+++ shopify_api_client_cli/src/cli/cli_steps.rs
@@ -0,0 +1,9 @@
+// TODO:
+
+pub fn first_step_login() {}
+
+pub fn second_step_what_do_you_want() {}
+
+pub fn third_step_selecting_products() {}
+
+pub fn forth_step_checking_selected_products() {}
--- shopify_api_client_cli/src/main.rs
+++ shopify_api_client_cli/src/main.rs
@@ -1,3 +1,4 @@
+mod cli;
 mod graphqls;
 mod models;
 mod render_templates;
